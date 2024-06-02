aa8fe36808aaf7bf0d01afef824c895e4dbbe94a
deepxanshu <deepanshusejwar@gmail.com>
2024-06-03 02:51:07 +0530

    vercel json config

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://f88e-2401-4900-1c68-ad1b-9ccb-5226-5615-51ad.ngrok-free.app';
+const API_URL = 'https://further-ai-backend.vercel.app';
 
 export const sendMessage = async (chatId: number, message: string) => {
     const response = await axios.post(`${API_URL}/chats/${chatId}/messages`, 
